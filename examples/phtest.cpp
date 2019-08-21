fcba5cd3f1012e317a8c74f12cb0f2066c094707
Johannes Kern <johannes.kern@zoho.com>
2019-08-21 14:42:30 +0200

    Added status bar function

--- examples/phtest.cpp
+++ examples/phtest.cpp
@@ -19,9 +19,13 @@ int main(){
         int avrows,avcols;
         int xoff,yoff; 
         int maxrows,maxcols;
+	std::vector<ph::HTMParam> status;
+	
 
         setlocale(LC_ALL, "");
         initscr();
+	use_default_colors();
+	start_color();		
         getmaxyx(stdscr,avrows,avcols);
 
         maxcols = avcols < NCOLS << 1 ? avcols : NCOLS << 1;
@@ -30,16 +34,37 @@ int main(){
         yoff = (avrows - maxrows) >> 1;
         xoff = (avcols - maxcols) >> 1; 
 
-        //mvprintw(avrows-2,0,"xoff = %d yoff = %d \n", xoff, yoff); 
-        //mvprintw(avrows-3,0,"avcols= %d avrows = %d \n", avcols,avrows); 
-        //mvprintw(avrows-4,0,"maxcols= %d maxrows = %d \n", maxcols,maxrows); 
+	init_pair(1, COLOR_BLACK, COLOR_WHITE);
+	
+//        mvprintw(avrows-2,0,"xoff = %d yoff = %d \n", xoff, yoff); 
+//        mvprintw(avrows-3,0,"avcols= %d avrows = %d \n", avcols,avrows); 
+//        mvprintw(avrows-4,0,"maxcols= %d maxrows = %d \n", maxcols,maxrows); 
 
+	ph::HTMParam idxParam = {"IDX","%.0f",0};
+	status.push_back(idxParam); 
+	
+	size_t numcat = 2;
+	size_t enclen = 4;
+	size_t phorizon = 10; 
 
+	th::CategoryEncoder encoder(numcat, enclen);
+	for(int i=0; i<phorizon; i++) {
+		auto sdr = encoder.encode(i%numcat);
+		status[0].value +=1;
+		ph::printStatus(status,avrows,avcols);
+		ph::printSDR(sdr,maxrows,maxcols,xoff,yoff);
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));	
+		refresh();
+	}
 
-	simpleDemo(maxrows, maxcols, xoff,yoff); 
+	if(has_colors() == FALSE)
+	{	endwin();
+		printf("Your terminal does not support color\n");
+		exit(1);
+	}
+	
 
-
-
+//	simpleDemo(maxrows, maxcols, xoff,yoff); 
 
         getch();
         endwin();
@@ -51,7 +76,7 @@ int simpleDemo(int maxrows, int maxcols, int xoff, int yoff) {
 	int i;
 	th::ScalarEncoder scalarEncoder(0,100,SDR_SIZE,12);
 	for(i = 0; i<100; i++) {
-                auto sdr = scalarEncoder.encode(std::rand()%100);
+		xt::xarray<bool> sdr = scalarEncoder.encode(std::rand()%100);
         	ph::printSDR(sdr,maxrows,maxcols,xoff,yoff);
                 std::this_thread::sleep_for(std::chrono::milliseconds(1000));
                 refresh();
--- plot_htm/plot_htm.hpp
+++ plot_htm/plot_htm.hpp
@@ -14,10 +14,27 @@ namespace ph {
 const std::string ACTIVE{"\u25A0"};
 const std::string INACTIVE{"\u25A1"};
 
+enum HTMParamKey {
+	IDX,
+	ADY
 
+};
 
-inline void printStatus(const int avrows, const int avcols) {
+struct HTMParam {
+	std::string name;
+	std::string precision;
+	float value;
+};
+
+inline void printStatus(const std::vector<HTMParam> & status, const int avrows, const int avcols) {
+	attron(COLOR_PAIR(1));
+	std::stringstream ss; 
+	ss << status[0].name << " = " << status[0].precision; 
+	const std::string& tmp = ss.str();   
+	mvprintw(avrows-2,0,tmp.c_str(), status[0].value);
+	attroff(COLOR_PAIR(1));
 }	
+
 
 inline void printSDR(const xt::xarray<bool> & sdr, const int maxrows, const int maxcols, const int xoff, const int yoff){
 	int i; 
@@ -36,6 +53,7 @@ inline void printSDR(const xt::xarray<bool> & sdr, const int maxrows, const int 
                 }
 	}	
 }
+
 
 
 }
