3f45c43a117e6d86e1081b1cd81420a22f13959f
Johannes Kern <johannes.kern@zoho.com>
2019-08-29 15:53:48 +0200

    Testing ignored build folder

---
+++ examples/build/CMakeFiles/phtest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/phtest.dir/phtest.cpp.o"
+  "phtest"
+  "phtest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/phtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ examples/build/CMakeFiles/phtest.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for phtest.
+# This may be replaced when dependencies are built.
