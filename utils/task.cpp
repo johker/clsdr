e6b2d23418defcddad9dcd839fcb44528b78c7c7
Johannes Kern <johannes.kern@zoho.com>
2020-01-30 20:08:34 +0100

    Added worker function to task

--- utils/task.cpp
+++ utils/task.cpp
@@ -3,18 +3,21 @@
 Task::~Task() {
 	stopTask();
 }
-Task::Task(std::string argTaskName){
+explicit Task::Task(std::string argTaskName){
 		
 }
 bool startTask() {
-	shouldQuit = false; 
+	done = false; 
 	std::lock_guard<std::mutex> guard(messageQueueMutex);
 	workerThread = new std::thread(Task::worker, this);
 	std::this_thread::sleep_for(std::chrono::milliseconds(5));
 	return true;
 }
 bool stopTask() {
-	shouldQuit = true;
+	if(!workerThread) {
+		return true;
+	}
+	done = true;
 	messageQueuedCondition.notify_one();
 	workerThread->join();
 	workerThread = nullptr;
@@ -23,6 +26,7 @@ bool stopTask() {
 	{
 		messageQueue.pop(); 
 	}
+	return true;
 }
 bool Task::pushMessage(std::shared_ptr<T> argMessage) {
 	//TODO Prevent message overflow / diagnostics
@@ -30,5 +34,33 @@ bool Task::pushMessage(std::shared_ptr<T> argMessage) {
 	messageQueuedCondition.notify_one();
 	return true;
 }
+static int worker(Task* argTask) {
+	do {
+		{
+			std::unique_lock<std::mutex> lock(argTask->messageQueueMutex);
+			if(argTask->messageQueue.size() == 0) {
+				messageQueuedCondition.wait(lock);
+			}
+		}
+		std::shared_ptr<T> message = nullptr;
+		{
+			std::unique_lock<std::mutex> lock(argTask->messageQueueMutex);
+			if(argTask->messageQueue.size() > 0) {
+				message = argTask->messageQueue.front();
+				argTask->messageQueue.pop();
 
+			}
+		}
 
+		if(message != nullptr) {
+			argTask->handleMessage(message);
+		}
+
+	} while (!argTask->done);	
+
+	{
+		std::unique_lock<std::mutex> lock(argTask->messageQueueMutex); 
+		argTask->messageQueue = std::queue<std::shared_ptr<T>>();
+	}
+	return 0;
+}
--- utils/task.hpp
+++ utils/task.hpp
@@ -19,11 +19,12 @@ protected:
 	virtual bool handleMessage(std::shared_ptr<T> argMessage) = 0;
 
 private: 
-	std::atomic<bool> shouldQuit; 
+	std::atomic<bool> done; 
 	std::unique_ptr<std::thread> workerThread; 
 	mutable std::mutex messageQueueMutex;
 	std::queue<std::shared_ptr<T>> messageQueue;
 	std::condition_variable messageQueuedCondition;
+	static int worker(Task* argTask); 
 }
 
-}
+
